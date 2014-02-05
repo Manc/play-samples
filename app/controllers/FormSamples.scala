083f7d61454d687eaa7c9aeda022e06d058927eb
Nick Zahn <nic.z@me.com>
2014-02-05 08:10:38 +0000

    Simple form with simple validation is working

--- app/controllers/FormSamples.scala
+++ app/controllers/FormSamples.scala
@@ -9,9 +9,9 @@ object FormSamples extends Controller {
 
 	case class Form1Data(email: String, age: Int)
 
-	val form = Form(
+	val form1 = Form(
 		mapping(
-			"email" -> nonEmptyText,
+			"email" -> email,
 			"age" -> number(min = 0, max = 100)
 		)(Form1Data.apply)(Form1Data.unapply)
 	)
@@ -21,14 +21,14 @@ object FormSamples extends Controller {
 	 * Display a HTML form
 	 */
 	def getform1 = Action {
-		Ok(views.html.formsamples.getform1(form))
+		Ok(views.html.formsamples.getform1(form1))
 	}
 
 	/**
 	 * Handle form submission
 	 */
 	def postform1 = Action { implicit request =>
-		form.bindFromRequest.fold(
+		form1.bindFromRequest.fold(
 			formWithErrors => BadRequest(views.html.formsamples.getform1(formWithErrors)), // BadRequest("Not OK"), 
 			formData => Ok("OK, the email address is: " + formData.email)
 		)
