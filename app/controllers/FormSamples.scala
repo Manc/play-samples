72c9e288090a7328a4669841e95631f56f39035f
Nick Zahn <nic.z@me.com>
2014-02-06 07:24:25 +0000

    Rename form 1 view and add some comments

--- app/controllers/FormSamples.scala
+++ app/controllers/FormSamples.scala
@@ -7,8 +7,10 @@ import play.api.data.Forms._
 
 object FormSamples extends Controller {
 
+	// Case class to hold form data
 	case class Form1Data(email: String, age: Int)
 
+	// Form defines the structure of the form and basic validation rules
 	val form1 = Form(
 		mapping(
 			"email" -> email,
@@ -21,15 +23,16 @@ object FormSamples extends Controller {
 	 * Display a HTML form
 	 */
 	def getform1 = Action {
-		Ok(views.html.formsamples.getform1(form1))
+		Ok(views.html.formsamples.form1(form1))
 	}
 
 	/**
 	 * Handle form submission
 	 */
 	def postform1 = Action { implicit request =>
+		// Validate the form and pass the result to a view
 		form1.bindFromRequest.fold(
-			formWithErrors => BadRequest(views.html.formsamples.getform1(formWithErrors)), // BadRequest("Not OK"), 
+			formWithErrors => BadRequest(views.html.formsamples.form1(formWithErrors)),
 			formData => Ok("OK, the email address is: " + formData.email)
 		)
 	}
